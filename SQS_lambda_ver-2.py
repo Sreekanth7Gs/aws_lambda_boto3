0e8767d0a978baf44c6a835c942d64fad560f721
gollasreekanth <96763290+gollasreekanth@users.noreply.github.com>
2023-11-28 12:03:47 +0530

    Update SQS_lambda_ver-2.py

--- SQS_lambda_ver-2.py
+++ SQS_lambda_ver-2.py
@@ -3,7 +3,6 @@ import boto3
 
 def lambda_handler(event, context):
     sns_client = boto3.client('sns', region_name='us-east-1')
-    ses_client = boto3.client('ses', region_name='us-east-1')
 
     for record in event['Records']:
         try:
@@ -12,51 +11,31 @@ def lambda_handler(event, context):
             message_body = json.loads(record['body'])
             
             status = message_body.get('status', 'default').lower()
-            message_content = message_body.get('content', 'No content')
 
             if status == 'refund':
                 sns_topic_arn = 'arn:aws:sns:us-east-1:882457892107:refund_sns' 
-                to_email = 'manoj11223s@gmail.com'
-                send_email(ses_client, to_email, message_body)
-                sns_client.publish(
-                    TopicArn=sns_topic_arn,
-                    Message=json.dumps(message_body),
-                )
+                publish_message(sns_client, sns_topic_arn, message_body)
 
             elif status == 'order':
                 sns_topic_arn = 'arn:aws:sns:us-east-1:882457892107:order_sns'
-                to_email = 'gssreekanth21@gmail.com'
-                send_email(ses_client, to_email, message_body)
-                sns_client.publish(
-                    TopicArn=sns_topic_arn,
-                    Message=json.dumps(message_body),
-                )
+                publish_message(sns_client, sns_topic_arn, message_body)
 
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
-            # Handle JSON decoding error
+           
         except Exception as e:
             print(f"Unexpected error: {e}")
-            # Handle other unexpected errors
+            
 
-def send_email(ses_client, to_email, message_body):
-    ses_sender_email = "gssreekanth016@gmail.com"
-
-    subject = f"Notification for status: {message_body.get('status', 'Unknown')}"
-    body = f"Message Content:\n{message_body.get('content', 'No content')}"
-
+def publish_message(sns_client, sns_topic_arn, message_body):
     try:
-        response = ses_client.send_email(
-            Source=ses_sender_email,
-            Destination={'ToAddresses': [to_email]},
-            Message={
-                'Subject': {'Data': subject},
-                'Body': {'Text': {'Data': body}}
-            }
+        sns_response = sns_client.publish(
+            TopicArn=sns_topic_arn,
+            Message=json.dumps(message_body),
         )
 
-        print(f"Email sent to {to_email}. Response: {response}")
+        print(f"Message published to {sns_topic_arn}. Response: {sns_response}")
 
     except Exception as e:
-        print(f"Error sending email: {e}")
-        # Handle email sending error
+        print(f"Error publishing message: {e}")
+       
